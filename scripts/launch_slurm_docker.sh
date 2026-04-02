f8865758a7a09533ef58b1ba18f3e7c9ba4ad89e
Chris Cai <chcai@nova-login-gtu2.prov.gtu.zts.cpe.ice.amd.com>
2026-04-02 03:03:49 +0000

    clean up

--- scripts/launch_slurm_docker.sh
+++ scripts/launch_slurm_docker.sh
@@ -241,11 +241,10 @@ srun --kill-on-bad-exit=1 --export=ALL bash -c '
                 echo AINIC_enabled
             fi
 
-            mkdir -p /tmp/dockerhome
-            pip install --user --no-cache-dir polars pyarrow pyyaml tqdm datasets pytest psutil
+            pip install --no-cache-dir polars pyarrow pyyaml tqdm datasets pytest psutil
             # Distributed Shampoo optimizer -- requires Python 3.12+
             # Only installed when config contains dense_optimizer: shampoo
-            grep -q shampoo /workspace/dlrm/$CONFIG 2>/dev/null && pip install --user --no-cache-dir git+https://github.com/facebookresearch/optimizers.git && echo distributed-shampoo_installed
+            grep -q shampoo /workspace/dlrm/$CONFIG 2>/dev/null && pip install --no-cache-dir git+https://github.com/facebookresearch/optimizers.git && echo distributed-shampoo_installed
             export PYTHONPATH=/workspace/dlrm:\${PYTHONPATH:-}
             torchrun \
                 --nproc_per_node='$GPUS' \
